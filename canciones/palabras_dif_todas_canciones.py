9b1f2c2e62d453085a8206597300c14d1b6104b3
Dlassoc <99360471+Dlassoc@users.noreply.github.com>
2022-10-27 17:12:15 -0500

    Delete canciones directory

--- canciones/palabras_dif_todas_canciones.py
+++
@@ -1,8 +0,0 @@
-x = open("canciones.txt", encoding="utf8")
-hola = x.read().split()
-palabras_diff = []
-for i in hola:
-    for j in hola:
-        if i != j and  i not in palabras_diff:
-            palabras_diff.append(i)
-print(f"las palabras diferentes son {len(palabras_diff)}")
